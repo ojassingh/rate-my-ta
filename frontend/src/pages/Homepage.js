c9ed7ca1a1177a599eedd854d4904ae8bf315d22
Yuval Kamani <yuvalkamani@gmail.com>
2023-05-15 11:20:55 -0400

    Implemented the home page

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,9 +1,23 @@
 import React from "react";
+import "./Homepage.css";
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="main-container">
+      <div className="home-title">Rate my TA.</div>
+      <div className="home-subtitle">Select your campus to get started</div>
+      <div className="campus-container">
+        <div>
+          <input type="checkbox" id="utm" name="utm" value="utm" /> UTM
+        </div>
+        <div>
+          <input type="checkbox" id="utsg" name="utsg" value="utsg" /> UTSG
+        </div>
+        <div>
+          <input type="checkbox" id="utsc" name="utsc" value="utsc" /> UTSC
+        </div>
+      </div>
+      <div className="home-ta-select">I'd like to look up the TA by name</div>
     </div>
   );
 };
