7cb76a4018e37f8f6a3a33aae1a8b79d962dd775
Yuval Kamani <yuvalkamani@gmail.com>
2023-05-15 10:14:55 -0400

    Added navigation bar and pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,22 @@
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div>Rate my TA</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
