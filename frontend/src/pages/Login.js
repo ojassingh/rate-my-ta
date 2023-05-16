39699803d3fd38299b7b3401beff16854a7937a6
Yuval Kamani <yuvalkamani@gmail.com>
2023-05-16 15:13:11 -0400

    Created the login form and frontend design for it

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,25 @@
 import React from "react";
+import "./Login.css";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login-container">
+      <h1>LogIn</h1>
+      <form>
+        <div className="login-form-container">
+          <div className="login-inner-container">
+            <label className="login-form-label">Email</label>
+            <input className="login-form-input" type="email" />
+          </div>
+          <div className="login-inner-container">
+            <label className="login-form-label">Password</label>
+            <input className="login-form-input" type="password" />
+          </div>
+        </div>
+        <button type="submit" className="login-submit">
+          Log In
+        </button>
+      </form>
     </div>
   );
 };
